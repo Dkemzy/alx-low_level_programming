8c1347eddc6e1794339e0f1e00166b617244b715
Dkemzy <Dennisworks366@gmail.com>
2022-06-08 06:47:45 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-Entry Point
+ * main.h contains function needed to execute commands
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x='a';x <='z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main-Entrypoint
+ * main.h contains functions needed for these tasks
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int alphabet_count = 0;
+	{
+		char m='a';
+
+		while (m <= 'z')
+		{
+			_putchar(m);
+			m++;
+		}
+		alphabet_count++;
+		_putchar('\n')
+	}
+}
