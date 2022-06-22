a219664b1761e6c8894afa42cad839610e6dc346
Dkemzy <Dennisworks366@gmail.com>
2022-06-22 03:45:29 -0700

    Update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion -  a function that prints a string.
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion is similar to factorial
