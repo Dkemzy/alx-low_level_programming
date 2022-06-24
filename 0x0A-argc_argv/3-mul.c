31c1dd3cdea778ec42b72ed23331ebe352af43f7
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-06-24 22:13:58 +0300

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,15 @@
  * @argv: Array name
  * Return: 1 if not enough arguments passed in, 0 otherwise
  */
-int main(int argc, char *arg[])
+
+int main(int argc, char *argv[])
 {
 	int i, j;
 
 	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
