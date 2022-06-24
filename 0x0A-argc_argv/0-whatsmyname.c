a83c146fc2265b047d0baa7237a5e1f114832e54
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-06-24 22:08:06 +0300

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Prints the name of the program
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n,argv[argc * 0]);
+	printf("%s\n", argv[argc * 0]);
 
 	return (0);
 }
