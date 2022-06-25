14a3c2acdac152225864845a9ab0a97d6d9f88ac
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-06-25 23:15:01 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*copy list from task*/
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,6 +22,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-
+char *_strstr(char *haystack, char *needle)
+  
 #endif
