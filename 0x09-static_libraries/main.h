9a71d01139fa88cf5417104b0b3257a70f368953
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-06-25 23:17:32 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -11,6 +11,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
+char *_strchr(char *s, char c)
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
