2f3208dae110d095e9b4c39f7c83c2bd464c62ed
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-06-27 22:37:25 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,85 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * *argstostr - convert arguments on command line to strings
- * @ac: int type
- * @av: pointer to array
- * Return: arguments as strings
+ * strtow - char
+ * @str: pointer to string params
+ * Return: char
  */
 
-char *argstostr(int ac, char **av)
+char **strtow(char *str)
 {
-	int size, count, count1, count2 = 0;
-	char *ptr;
+	int i = 0, j = 0, k = 0;
+	int len = 0, count = 0;
+	char **f, *col;
 
-	if (ac == 0 || av == NULL)
+	if (!str || !*str)
 	{
 		return (NULL);
 	}
 
-	for (count = 0; count < ac; count++)
+	while (*(str + i))
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		if (*(str + i) != ' ')
 		{
-			size += 1;
+			if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
+			{
+				count += 1;
+			}
 		}
-		size += 1;
+		i++;
 	}
-	size += 1;
 
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
+	if (count == 0)
 	{
-		free(ptr);
 		return (NULL);
 	}
-	for (count = 0; count < ac; count++)
+	count += 1;
+	f = malloc(sizeof(char *) * count);
+
+	if (!f)
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			ptr[count2] = av[count][count1];
-			count2++;
-		}
-		ptr[count2] = '\n';
-		count2++;
+		return (NULL);
 	}
-	ptr[count2] = '\0';
-	return (ptr);
-}
+	i = 0;
+
+	while (*str)
+	{
+		while (*str == ' ' && *str)
+		{
+			str++;
+		}
+		len = 0;
+
+		while (*(str + len) != ' ' && *(str + len))
+		{
+			len += 1;
+		}
+		len += 1;
+		col = malloc(sizeof(char) * len);
+
+		if (!col)
+		{
+			for (k = j - 1; k >= 0; k--)
+			{
+				free(f[k]);
+			}
+			free(f);
+			return (NULL);
+		}
+
+		for (k = 0; k < (len - 1);  k++)
+		{
+			*(col + k) = *(str++);
+		}
+		*(col + k) = '\0';
+		*(f + j) = col;
+
+		if (j < (count - 1))
+		{
+			j++;
+		}
+	}
+	*(f + j) = NULL;
+	return (f);
+} /*yes*/
+
