42b5f85abd4a10fb918bb6c2a1a43458267cae69
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-09-23 23:36:09 +0300

    Harsh

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,16 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - function that generates a distributed index
- * according to a given hash
- *
- * @key: key passed
- * @size: size of the hash tables
- * Return: key index
- */
+* key_index - give he index of a key
+* @key: the key
+* @size: size of array
+* Return: index for the key ,short value
+*/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == 0)
-		return (0);
-	return (hash_djb2(key) % size);
+	return ((hash_djb2(key)) % size);
 }
