67e832b4699f87c756e3320837297871916d7bbb
Dkemzy <Dennisworks366@gmail.com>
2023-02-11 18:43:30 +0000

    search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,14 @@
-#
+#include "search_algos.h"
+
+/**
+* recursive_search - searches for a value in an array of integers using the
+* Binary search algorithm
+*
+* @array: input array
+* @size: size of the array
+* @value: value to search for
+* Return: index of the number
+*/
+int recursive_search(int *array, size_t size, int value)
+{
+ 	size_t half = size / 2
